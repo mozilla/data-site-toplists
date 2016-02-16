6d0436b0604ac601b88900bbcc90ef07474522bc
Hallvord R. M. Steen <hsteen@mozilla.com>
2016-02-16 22:56:09 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # data-site-toplists
 Various lists of important sites, mostly by region
+
+These lists of sites we consider important help power reporting of web compatibility issues in various sites and test frameworks - especially [arewecompatibleyet.com](http://arewecompatibleyet.com) and [Compatipede](https://github.com/mozilla/compatipede).
+
+The lists are sourced from site ranking services like Alexa and Similarweb.com, but they are also curated with input from volunteers in various locales: certain domains that have high rankings but are noise are removed, domains the contributors consider important for whatever reason are added.
+
+Basically, the goals for the editing were:
+
+* Remove "noise" like t.co and googleusercontent.com - domains that are of no real independent importance
+* Remove spam sites and link farms that achieved artificial high scores (and important problem in smaller locales)
+* Remove major global sites from local listings. Trying to focus on the most important sites in a given country is less interesting when issues for Facebook and Google clog up all the data collection.
+
+We've also attempted to remove NSFW sites. This is mostly to ensure our projects and activities are inclusive and welcoming everyone - also those who for whatever reasons prefer to avoid NSFW sites and screenshots of those.
+
+Editing the lists is helpful because it helps us focus on important sites and prioritise problems that can have a big impact. **If you would like to suggest a change to one of the lists we welcome pull requests.**
+
---
+++ toplists.js
@@ -0,0 +1,186 @@
+/* Order is important: US first, then top-1000
+This matters due to filtering logic excluding US
+sites from other listings on arewecompatibleyet.com
+*/
+var topLists = [
+{
+     "name": "U.S. 50",
+     "url": "bycountry/us50.json",
+     "id": "us50"
+    },
+    {
+     "name": "Top 1000",
+     "url": "bycountry/1000.json",
+     "id": "1000"
+    },
+    {
+     "name": "Albania top 90",
+     "url": "bycountry/al90.json",
+     "id": "al90"
+    },
+    {
+     "name": "Argentina top 90",
+     "url": "bycountry/ar90.json",
+     "id": "ar90"
+    },
+    {
+     "name": "Bangladesh top sites",
+     "url": "bycountry/bd280.json",
+     "id": "bd280"
+    },
+    {
+     "name": "Brazil top 250",
+     "url": "bycountry/br50.json",
+     "id": "br50"
+    },
+    {
+     "name": "Bulgaria top sites",
+     "url": "bycountry/bg250.json",
+     "id": "bg250"
+    },
+    {
+     "name": "Canada top 50",
+     "url": "bycountry/ca50.json",
+     "id": "ca50"
+    },
+    {
+     "name": "Chile top 100",
+     "url": "bycountry/cl100.json",
+     "id": "cl100"
+    },
+    {
+     "name": "China top 50",
+     "url": "bycountry/cn50.json",
+     "id": "cn50"
+    },
+    {
+     "name": "Colombia top sites",
+     "url": "bycountry/co50.json",
+     "id": "co50"
+    },
+    {
+     "name": "Croatia top 100",
+     "url": "bycountry/hr100.json",
+     "id": "hr100"
+    },
+    {
+     "name": "Czech top 100",
+     "url": "bycountry/cz100.json",
+     "id": "cz100"
+    },
+    {
+     "name": "Germany top 50",
+     "url": "bycountry/de50.json",
+     "id": "de50"
+    },
+    {
+     "name": "France top 100",
+     "url": "bycountry/fr100.json",
+     "id": "fr100"
+    },
+    {
+     "name": "Greece top 100",
+     "url": "bycountry/gr100.json",
+     "id": "gr100"
+    },
+    {
+     "name": "Hungary toplist",
+     "url": "bycountry/hu250.json",
+     "id": "hu250"
+    },
+    {
+     "name": "India top 100",
+     "url": "bycountry/in100.json",
+     "id": "in100"
+    },
+    {
+     "name": "Italy top 50",
+     "url": "bycountry/it50.json",
+     "id": "it50"
+    },
+    {
+     "name": "Japan top 50",
+     "url": "bycountry/jp50.json",
+     "id": "jp50"
+    },
+    {
+     "name": "Korea top 50",
+     "url": "bycountry/kr50.json",
+     "id": "kr50"
+    },
+    {
+     "name": "Mexico top sites",
+     "url": "bycountry/mx50.json",
+     "id": "mx50"
+    },
+    {
+     "name": "Norway top 70",
+     "url": "bycountry/no70.json",
+     "id": "no70"
+    },
+    {
+     "name": "Peru top 270",
+     "url": "bycountry/pe270.json",
+     "id": "pe270"
+    },
+    {
+     "name": "Poland top 50",
+     "url": "bycountry/pl50.json",
+     "id": "pl50"
+    },
+    {
+     "name": "Portugal top 50",
+     "url": "bycountry/pt50.json",
+     "id": "pt50"
+    },
+    {
+        "name": "Russia top sites",
+        "url": "bycountry/ru.json",
+        "id": "ru"
+    },
+    {
+        "name": "South Africa 150",
+        "url": "bycountry/za150.json",
+        "id": "za150"
+    },
+    {
+     "name": "Spain top 50",
+     "url": "bycountry/es50.json",
+     "id": "es50"
+    },
+    {
+        "name": "Sweden 60",
+        "url": "bycountry/se60.json",
+        "id": "se60"
+    },
+    {
+     "name": "UK 50",
+     "url": "bycountry/uk50.json",
+     "id": "uk50"
+    },
+    {
+     "name": "Uruguay 90",
+     "url": "bycountry/uy90.json",
+     "id": "uy90"
+    },
+    {
+     "name": "Venezuela 50",
+     "url": "bycountry/ve50.json",
+     "id": "ve50"
+    },
+    {
+     "name": "Mail",
+     "url": "bytopic/mail.json",
+     "id": "mail"
+    },
+    {
+     "name": "Top video sites",
+     "url": "bytopic/topvideosites.json",
+     "id": "topvideosites"
+    },
+    {
+     "name": "Top Moz BD sites",
+     "url": "bytopic/mbd.json",
+     "id": "mbd"
+    }
+]
